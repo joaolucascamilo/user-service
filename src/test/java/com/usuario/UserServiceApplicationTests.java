701924d2e9e880e56b3587d6b9bd6147427343fb
jlucascamilo <lucascamilo373@gmail.com>
2026-07-01 14:19:43 -0300

    fix: ajustes para build e testes unitários

--- src/test/java/com/usuario/UserServiceApplicationTests.java
+++ src/test/java/com/usuario/UserServiceApplicationTests.java
@@ -2,8 +2,13 @@ package com.usuario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"brevo.api.key=test-key",
+		"brevo.remetente.email=test@example.com"
+})
 class UserServiceApplicationTests {
 
 	@Test
